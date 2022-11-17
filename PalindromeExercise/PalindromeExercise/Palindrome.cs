65a02883e63c93b54614aa34dc9c9e8e0ef256a9
TylerN93 <tylernewton73@gmail.com>
2022-11-16 21:30:33 -0600

    Completed

---
+++ PalindromeExercise/PalindromeExercise/Palindrome.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class Wordsmith
+    {
+        public Wordsmith()
+        {
+        }
+
+        public bool IsAPalendrome(string word)
+        {
+            char[] reversed = word.ToCharArray();
+            Array.Reverse(reversed);
+            string s = new string(reversed);
+
+
+            if (word == s)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,24 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("car", false)]
+        public void MyTest(string word, bool expected)
         {
+            var test = new Wordsmith();
+
+            var actual = test.IsAPalendrome(word);
+
+            Assert.Equal(expected, actual);
+
+
+
 
         }
     }
